e8ca0de7bb4987a2a50763ef2927df34704a1cf6
Nato Boram <NatoBoram@users.noreply.github.com>
2016-11-19 13:37:06 -0500

    Create FirstRun.sh

---
+++ Windows 10/HowTo/Bash/FirstRun.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Graphical User Interface
+echo "export DISPLAY=:0.0" >> ~/.bashrc
+sudo sed -i 's/<listen>.*<\/listen>/<listen>tcp:host=localhost,port=0<\/listen>/' /etc/dbus-1/session.conf
+
+# Update
+sudo aptitude update
+sudo aptitude full-upgrade
+
+# Upgrade
+lsb_release -a
+sudo aptitude install update-manager-core
+sudo do-release-upgrade
+sudo dpkg --configure -a
+lsb_release -a
+
+# Software
+sudo aptitude install ubuntu-desktop
+sudo aptitude install unity
+sudo aptitude install compiz-core
+sudo aptitude install compizconfig-settings-manager
+
+# In case something broke
+sudo dpkg-reconfigure locales
+sudo chown -Rv $USER:$USER ~
+
+echo GENERAL
+echo Commands
+echo Composite
+echo Copy to texture
+echo OpenGL
+
+echo DESKTOP
+echo Ubuntu Unity Plugin
+
+echo IMAGE LOADING
+echo PNG
+
+echo UTILITY
+echo Compiz Library Toolbox
+
+echo WINDOWS MANAGEMENT
+echo Move Window
+echo Place Windows
+echo Resize Window
+echo Scale
+echo Snapping Windows
+
+# Unity
+sudo ccsm
+sudo compiz
