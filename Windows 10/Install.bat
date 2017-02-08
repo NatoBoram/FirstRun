3bc52122111189c0a04a6a455dea3453db96dfe3
Nato Boram <NatoBoram@users.noreply.github.com>
2017-02-07 22:25:36 -0500

    Update Install.bat

--- Windows 10/Install.bat
+++ Windows 10/Install.bat
@@ -3,7 +3,7 @@ echo Help installing softwares on new Windows 10 computers.
 pause
 
 echo Google Chrome
-start "Google Chrome" "https://www.google.ca/chrome/browser/desktop/index.html"
+start "Google Chrome" "https://www.google.ca/chrome/browser/canary.html"
 pause
 
 echo Drivers
@@ -26,10 +26,12 @@ echo Applications
 start chrome
 ping 127.0.0.1 > nul
 start "Audacity" "http://audacityteam.org/download/"
+start "FreeRapid" "http://wordrider.net/freerapid/download.htm"
 start "Gimp" "http://www.gimp.org/downloads/"
-start "iTunes" "http://www.apple.com/itunes/download/"
-start "LibreOffice" "http://www.libreoffice.org/download/libreoffice-fresh/?type=win-x86_64&lang=fr"
+start "Krita" "https://krita.org/en/download/krita-desktop/"
+start "LibreOffice" "http://www.libreoffice.org/download/libreoffice-fresh/?type=win-x86_64&version=5.3.0&lang=fr"
 start "Notepad++" "https://notepad-plus-plus.org/download/"
+start "QBitTorrent" "https://www.qbittorrent.org/download.php"
 start "Skype" "http://www.skype.com/fr/download-skype/skype-for-computer/"
 start "VLC Media Player" "http://www.videolan.org/vlc/download-windows.html"
 pause
@@ -40,29 +42,41 @@ ping 127.0.0.1 > nul
 start "Cheat Engine" "http://www.cheatengine.org/"
 start "Discord" "https://discordapp.com/download"
 start "GeForce Experience" "http://www.geforce.com/geforce-experience"
-start "Razer Synapse" "http://www.razerzone.com/synapse/"
 start "Guild Wars 2" "https://account.arena.net/content"
 start "League of Legends" "http://na.leagueoflegends.com/"
-start "Minecraft" "https://minecraft.net/download"
+start "LoL Updater" "https://lolupdater.com/"
+start "Minecraft" "https://minecraft.net/fr-ca/download/"
+start "MSI AfterBurner" "https://www.msi.com/page/afterburner"
+start "Origin" "https://www.origin.com/can/en-us/store/download"
+start "Razer Surround" "http://www.razerzone.com/surround/"
+start "Razer Synapse" "http://www.razerzone.com/synapse/"
 start "Steam" "http://store.steampowered.com/about/"
 start "SystemRequirementsLab" "http://www.systemrequirementslab.com/cyri"
+start "uPlay" "https://uplay.ubi.com/"
 pause
 
 echo Computer Tools
 start chrome
 ping 127.0.0.1 > nul
+start "Android Studio" "https://developer.android.com/studio/index.html#downloads"
 start "AutoHotkey" "http://www.autohotkey.com/"
+start "Daemon Tools Lite" "http://www.disk-tools.com/download/daemon"
 start "Eclipse" "https://eclipse.org/downloads/"
 start "FileZilla" "https://filezilla-project.org/download.php"
+start "Firefox Developer Edition" "https://www.mozilla.org/en-US/firefox/developer/all/?q=French,%20Fran%C3%A7ais"
 start "GitHub" "https://desktop.github.com/"
 start "Git for Windows" "https://git-for-windows.github.io/"
+start "Windows 10" "https://www.microsoft.com/fr-ca/software-download/windows10"
+start "Windows 10 Start Fresh" "https://www.microsoft.com/fr-ca/software-download/windows10startfresh"
 start "PuTTY" "http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html"
-start "Python" "https://www.python.org/downloads/"
+start "Python" "https://www.python.org/downloads/windows/"
 start "SQL Developer" "http://www.oracle.com/technetwork/developer-tools/sql-developer/downloads/index.html"
 start "TCP Optimizer" "http://www.speedguide.net/downloads.php"
 start "Rufus" "https://rufus.akeo.ie/"
+start "VcXsrv" "https://sourceforge.net/projects/vcxsrv/"
 start "VirtualBox" "https://www.virtualbox.org/"
-start "Visual Studio" "https://www.visualstudio.com/"
+start "Visual Studio" "https://www.visualstudio.com/downloads/"
+start "Visual Studio Code" "https://code.visualstudio.com/"
 pause
 
 exit
