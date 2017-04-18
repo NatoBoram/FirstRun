d1919a5a779054ea904bc62f524d5a3e9c8ece1c
Nato Boram <NatoBoram@users.noreply.github.com>
2017-04-18 19:11:00 -0400

    Update FirstRun.sh

--- Windows 10/HowTo/Bash/FirstRun.sh
+++ Windows 10/HowTo/Bash/FirstRun.sh
@@ -5,49 +5,24 @@ echo "export DISPLAY=:0.0" >> ~/.bashrc
 sudo touch /etc/dbus-1/session.conf
 sudo sed -i 's/<listen>.*<\/listen>/<listen>tcp:host=localhost,port=0<\/listen>/' /etc/dbus-1/session.conf
 
+# Prepare for updates
+curl https://repogen.simplylinux.ch/txt/xenial/sources_3c003eb09793c9e81c0d8d8aa103264edeb0c9c0.txt | sudo tee /etc/apt/sources.list
+sudo nano /etc/update-manager/release-upgrades
+
 # Update
-sudo apt-get install aptitude
-sudo aptitude update
-sudo aptitude full-upgrade
+sudo apt update
+sudo apt full-upgrade
+sudo apt autoremove
 
 # Upgrade
 lsb_release -a
-sudo do-release-upgrade
+sudo do-release-upgrade -d
 sudo dpkg --configure -a
 lsb_release -a
 
-# Software
-sudo aptitude install ubuntu-desktop unity compizconfig-settings-manager compiz-core openjdk-8-jre openjdk-8-jdk freeglut3 git ffmpeg
+# Softwares
+sudo apt install adb openjdk-8-jdk git ffmpeg
 
 # In case something broke
 sudo dpkg-reconfigure locales
 sudo chown -Rv $USER:$USER ~
-
-# CCSM
-echo GENERAL
-echo Commands
-echo Composite
-echo Copy to texture
-echo OpenGL
-
-echo DESKTOP
-echo Expo
-echo Ubuntu Unity Plugin
-
-echo IMAGE LOADING
-echo PNG
-
-echo UTILITY
-echo Compiz Library Toolbox
-
-echo WINDOWS MANAGEMENT
-echo Move Window
-echo Place Windows
-echo Resize Window
-echo Scale
-echo Snapping Windows
-
-# Unity
-sudo ccsm
-sudo ccsm
-sudo compiz
