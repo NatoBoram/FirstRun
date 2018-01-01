74e468ec4fa57765a0ccb7890e1d1b300db685c1
Nato Boram <nato.boram@gmail.com>
2018-01-01 13:30:42 -0500

    IPFS, FFMPEG, Reboot

--- Linux/Ubuntu.sh
+++ Linux/Ubuntu.sh
@@ -6,8 +6,15 @@ sudo apt full-upgrade -y
 sudo apt autoremove -y
 
 # Install
-sudo apt install -y adb curl filezilla firefox gimp git golang default-jdk krita libreoffice qbittorrent steam vlc
+sudo apt install -y adb curl ffmpeg filezilla firefox gimp git golang default-jdk krita libreoffice qbittorrent steam vlc
 
 # [LAMP] Linux Apache MySQL PHP
 sudo apt install -y mysql-server
 sudo apt install -y phpmyadmin
+
+# Snap
+sudo snap install ipfs
+
+# Over!
+sudo do-release-upgrade
+reboot
