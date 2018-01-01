0e47ccb25681c1c163f9d4bc3d2be2cbb5b5873b
Nato Boram <nato.boram@gmail.com>
2018-01-01 12:40:22 -0500

    MySQL Server

--- Linux/Ubuntu.sh
+++ Linux/Ubuntu.sh
@@ -7,3 +7,7 @@ sudo apt autoremove -y
 
 # Install
 sudo apt install -y adb curl filezilla firefox gimp git golang default-jdk krita libreoffice qbittorrent steam vlc
+
+# [LAMP] Linux Apache MySQL PHP
+sudo apt install -y mysql-server
+sudo apt install -y phpmyadmin
