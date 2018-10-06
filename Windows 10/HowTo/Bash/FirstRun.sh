be12b96fa8812b67e983f453f8f684ff3a8dd738
Nato Boram <nato.boram@gmail.com>
2018-10-06 18:56:20 -0400

    removed update lines

--- Windows 10/HowTo/Bash/FirstRun.sh
+++ Windows 10/HowTo/Bash/FirstRun.sh
@@ -6,8 +6,8 @@ sudo touch /etc/dbus-1/session.conf
 sudo sed -i 's/<listen>.*<\/listen>/<listen>tcp:host=localhost,port=0<\/listen>/' /etc/dbus-1/session.conf
 
 # Prepare for updates
-curl https://repogen.simplylinux.ch/txt/xenial/sources_3c003eb09793c9e81c0d8d8aa103264edeb0c9c0.txt | sudo tee /etc/apt/sources.list
-sudo nano /etc/update-manager/release-upgrades
+# curl https://repogen.simplylinux.ch/txt/xenial/sources_3c003eb09793c9e81c0d8d8aa103264edeb0c9c0.txt | sudo tee /etc/apt/sources.list
+# sudo nano /etc/update-manager/release-upgrades
 
 # Update
 sudo apt update
@@ -21,7 +21,7 @@ sudo dpkg --configure -a
 lsb_release -a
 
 # Softwares
-sudo apt install adb openjdk-8-jdk git ffmpeg
+sudo apt install adb git ffmpeg
 
 # In case something broke
 sudo dpkg-reconfigure locales
