ac776de6502c0e1a68ae65288a3588a1580f77f3
Nato Boram <nato.boram@gmail.com>
2018-10-06 18:57:08 -0400

    Install.bat now relies on Scoop

--- Windows 10/Install.bat
+++ Windows 10/Install.bat
@@ -1,9 +1,10 @@
 @echo off
 echo Help installing softwares on new Windows 10 computers.
+echo This script assumes you've installed `scoop`.
 pause
 
 echo Firefox Developer Edition
-start "Firefox Developer Edition" "https://www.mozilla.org/fr/firefox/developer/all/"
+REM start "Firefox Developer Edition" "https://www.mozilla.org/fr/firefox/developer/all/"
 pause
 
 echo Firefox
@@ -23,16 +24,14 @@ pause
 echo Drivers
 start Firefox
 ping 127.0.0.1 > nul
-start "Adobe Air" "https://get.adobe.com/fr/air/"
-start "Adobe Flash Player" "https://get.adobe.com/fr/flashplayer/"
 start "Adobe Reader" "https://get.adobe.com/fr/reader/"
-start "Adobe Shockwave" "https://get.adobe.com/fr/shockwave/"
-start "DirectX" "https://www.microsoft.com/fr-ca/download/confirmation.aspx?id=35"
-start "Go" "https://golang.org/dl/"
-start "IPFS" "https://dist.ipfs.io/#go-ipfs"
-start "Java" "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
-start "Silverlight" "http://www.microsoft.com/silverlight/"
-start "7-Zip" "http://www.7-zip.org/"
+start "GeForce Experience" "http://www.geforce.com/geforce-experience"
+REM start "Go" "https://golang.org/dl/"
+REM start "IPFS" "https://dist.ipfs.io/#go-ipfs"
+REM start "Java" "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
+start "MSI AfterBurner" "https://www.msi.com/page/afterburner"
+start "Razer Surround" "http://www.razerzone.com/surround/"
+REM start "7-Zip" "http://www.7-zip.org/"
 pause
 
 echo Fonts
@@ -46,35 +45,27 @@ pause
 echo Applications
 start Firefox
 ping 127.0.0.1 > nul
-start "Audacity" "http://audacityteam.org/download/"
+REM start "Audacity" "http://audacityteam.org/download/"
+start "Discord" "https://discordapp.com/download"
 start "FreeRapid" "http://wordrider.net/freerapid/download.htm"
-start "Gimp" "http://www.gimp.org/downloads/"
-start "Krita" "https://krita.org/en/download/krita-desktop/"
-start "LibreOffice" "https://www.libreoffice.org/download/libreoffice-fresh/?type=win-x86_64"
-start "Notepad++" "https://notepad-plus-plus.org/download/"
-start "QBitTorrent" "https://www.qbittorrent.org/download.php"
-start "Rainmeter" "https://www.rainmeter.net/"
-start "Skype" "http://www.skype.com/fr/download-skype/skype-for-computer/"
-start "VLC Media Player" "http://www.videolan.org/vlc/download-windows.html"
+REM start "GIMP" "http://www.gimp.org/downloads/"
+REM start "Krita" "https://krita.org/en/download/krita-desktop/"
+REM start "LibreOffice" "https://www.libreoffice.org/download/libreoffice-fresh/?type=win-x86_64"
+REM start "QBitTorrent" "https://www.qbittorrent.org/download.php"
+REM start "Rainmeter" "https://www.rainmeter.net/"
+REM start "VLC Media Player" "http://www.videolan.org/vlc/download-windows.html"
 pause
 
 echo Games
 start Firefox
 ping 127.0.0.1 > nul
 start "Battle.net" "http://us.battle.net/en/app/"
-start "Cheat Engine" "http://www.cheatengine.org/"
-start "Discord" "https://discordapp.com/download"
-start "GeForce Experience" "http://www.geforce.com/geforce-experience"
+REM start "Cheat Engine" "http://www.cheatengine.org/"
 start "Guild Wars 2" "https://account.arena.net/content"
 start "League of Legends" "http://na.leagueoflegends.com/"
-start "LoL Updater" "https://lolupdater.com/"
 start "Minecraft" "https://minecraft.net/fr-ca/download/"
-start "MSI AfterBurner" "https://www.msi.com/page/afterburner"
-start "OBS Studio" "https://obsproject.com/download"
 start "Origin" "https://www.origin.com/can/en-us/store/download"
-start "Razer Surround" "http://www.razerzone.com/surround/"
-start "Razer Synapse" "http://www.razerzone.com/synapse/"
-start "Steam" "http://store.steampowered.com/about/"
+REM start "Steam" "http://store.steampowered.com/about/"
 start "System Requirements Lab" "http://www.systemrequirementslab.com/cyri"
 start "uPlay" "https://uplay.ubi.com/"
 pause
@@ -82,28 +73,26 @@ pause
 echo Computer Tools
 start Firefox
 ping 127.0.0.1 > nul
-start "Android Studio" "https://developer.android.com/studio/index.html#downloads"
-start "Atom" "https://atom.io/"
-start "AutoHotkey" "http://www.autohotkey.com/"
+REM start "Android Studio" "https://developer.android.com/studio/index.html#downloads"
+REM start "Atom" "https://atom.io/"
+REM start "AutoHotkey" "http://www.autohotkey.com/"
 start "Daemon Tools Lite" "http://www.disk-tools.com/download/daemon"
-start "Eclipse" "https://eclipse.org/downloads/"
-start "FileZilla" "https://filezilla-project.org/download.php"
-start "Firefox Developer Edition" "https://www.mozilla.org/en-US/firefox/developer/all/?q=French,%20Fran%C3%A7ais"
-start "GitHub" "https://desktop.github.com/"
-start "Git for Windows" "https://git-for-windows.github.io/"
+REM start "Eclipse" "https://eclipse.org/downloads/"
+REM start "FileZilla" "https://filezilla-project.org/download.php"
+REM start "Firefox Developer Edition" "https://www.mozilla.org/en-US/firefox/developer/all/?q=French,%20Fran%C3%A7ais"
+REM start "GitHub" "https://desktop.github.com/"
+REM start "Git for Windows" "https://git-for-windows.github.io/"
 start "Google Web Designer" "https://www.google.ca/webdesigner/"
 start "Windows 10" "https://www.microsoft.com/fr-ca/software-download/windows10"
-start "Windows 10 Start Fresh" "https://www.microsoft.com/fr-ca/software-download/windows10startfresh"
-start "PuTTY" "http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html"
-start "Python" "https://www.python.org/downloads/windows/"
+REM start "PuTTY" "http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html"
+REM start "Python" "https://www.python.org/downloads/windows/"
 start "SQL Developer" "http://www.oracle.com/technetwork/developer-tools/sql-developer/downloads/index.html"
-start "TCP Optimizer" "http://www.speedguide.net/downloads.php"
-start "Rufus" "https://rufus.akeo.ie/"
-start "VcXsrv" "https://sourceforge.net/projects/vcxsrv/"
-start "VirtualBox" "https://www.virtualbox.org/wiki/Downloads"
+REM start "Rufus" "https://rufus.akeo.ie/"
+REM start "VcXsrv" "https://sourceforge.net/projects/vcxsrv/"
+REM start "VirtualBox" "https://www.virtualbox.org/wiki/Downloads"
 start "Visual Studio" "https://www.visualstudio.com/downloads/"
-start "Visual Studio Code" "https://code.visualstudio.com/"
-start "Wamp Server" "http://www.wampserver.com/#download-wrapper"
+REM start "Visual Studio Code" "https://code.visualstudio.com/"
+REM start "Wamp Server" "http://www.wampserver.com/#download-wrapper"
 pause
 
 exit
