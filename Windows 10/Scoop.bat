4d210e7fd0186e9c7718825aac92cfd104764967
Nato Boram <nato.boram@gmail.com>
2018-10-06 20:45:58 -0400

    Scoop to batch

---
+++ Windows 10/Scoop.bat
@@ -0,0 +1,19 @@
+﻿@Echo Off
+
+REM Scoop
+PowerShell "Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')"
+
+REM Setup
+scoop install sudo
+sudo scoop install --global git openssh aria2
+
+REM Update
+scoop status
+scoop update
+
+REM Buckets
+scoop bucket add extras
+scoop bucket add java
+
+REM Install
+Start /Min PowerShell "sudo scoop install --global android-sdk android-studio autohotkey curl eclipse-java filezilla firefox-developer gimp github go go-ipfs libreoffice-fresh mediacreationtool msiafterburner nano nodejs openjdk python qbittorrent rainmeter rufus steam vlc vscode-insiders wget"
