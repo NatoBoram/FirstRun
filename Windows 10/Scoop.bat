d1554e6d4d225ece0a1888d30b289e49329770e3
Nato Boram <NatoBoram@users.noreply.github.com>
2018-11-29 11:30:10 -0500

    scoop bucket add games
    Added Minecraft to the installed apps.

--- Windows 10/Scoop.bat
+++ Windows 10/Scoop.bat
@@ -14,8 +14,9 @@ scoop update
 REM Buckets
 scoop bucket add extras
 scoop bucket add java
+scoop bucket add games
 
 REM Install
-Start /Min PowerShell "sudo scoop install --global android-sdk android-studio autohotkey curl eclipse-java filezilla firefox-developer gimp github go go-ipfs libreoffice-fresh mediacreationtool msiafterburner nano nodejs openjdk python qbittorrent rainmeter rufus steam vlc vscode-insiders wget"
+Start /Min PowerShell "sudo scoop install --global android-sdk android-studio autohotkey curl eclipse-java filezilla firefox-developer gimp github go go-ipfs libreoffice-fresh mediacreationtool msiafterburner nano nodejs openjdk python qbittorrent rainmeter rufus steam vlc vscode-insiders wget minecraft hyper"
 
 Exit
