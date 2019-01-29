584c84d8a97e1b399b3d781223dabd41c61ae957
Nato Boram <NatoBoram@users.noreply.github.com>
2019-01-28 19:29:49 -0500

    Update Scoop.bat

--- Windows 10/Scoop.bat
+++ Windows 10/Scoop.bat
@@ -4,8 +4,10 @@ REM Scoop
 PowerShell "Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')"
 
 REM Setup
-scoop install sudo
-sudo scoop install --global git openssh aria2
+scoop install --global git openssh aria2 sudo
+
+scoop config aria2-split 16
+scoop config aria2-max-connection-per-server 16
 
 REM Update
 scoop status
@@ -17,6 +19,6 @@ scoop bucket add java
 scoop bucket add games
 
 REM Install
-Start /Min PowerShell "sudo scoop install --global android-sdk android-studio autohotkey curl eclipse-java filezilla firefox-developer gimp github go go-ipfs libreoffice-fresh mediacreationtool msiafterburner nano nodejs openjdk python qbittorrent rainmeter rufus steam vlc vscode-insiders wget minecraft hyper neofetch"
+Start /Min PowerShell "sudo scoop install --global android-sdk android-studio autohotkey curl discord-canary eclipse-java filezilla firefox-developer gimp github go go-ipfs ipfs-desktop libreoffice-fresh mediacreationtool msiafterburner nano nodejs openjdk python qbittorrent rainmeter rufus steam vlc vscode-insiders wget minecraft hyper neofetch"
 
 Exit
