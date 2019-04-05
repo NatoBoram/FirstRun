d99413c1b7b01d0a3e4cfb159067ff1659995a33
Nato Boram <nato.boram+git@gmail.com>
2019-04-05 02:34:47 -0400

    multi-threaded scoop

--- Windows 10/Scoop.bat
+++ Windows 10/Scoop.bat
@@ -1,24 +1,62 @@
 ﻿@Echo Off
 
-REM Scoop
+Rem Scoop
 PowerShell "Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')"
 
-REM Setup
+Rem ---
+
+Rem Setup
 scoop install --global git openssh aria2 sudo
 
+Rem Config
 scoop config aria2-split 16
 scoop config aria2-max-connection-per-server 16
 
-REM Update
+Rem Update
 scoop status
 scoop update
 
-REM Buckets
+Rem Buckets
 scoop bucket add extras
 scoop bucket add java
 scoop bucket add games
 
-REM Install
-Start /Min PowerShell "sudo scoop install --global android-sdk android-studio autohotkey curl discord-canary eclipse-java filezilla firefox-developer gimp github go go-ipfs ipfs-desktop libreoffice-fresh mediacreationtool msiafterburner nano nodejs openjdk python qbittorrent rainmeter rufus steam vlc vscode-insiders wget minecraft hyper neofetch"
+Rem Install
+Start PowerShell scoop install --global android-sdk
+Start PowerShell scoop install --global android-studio
+Start PowerShell scoop install --global autohotkey
+Start PowerShell scoop install --global curl
+Start PowerShell scoop install --global discord-canary
+Start PowerShell scoop install --global eclipse-java
+Start PowerShell scoop install --global filezilla
+Start PowerShell scoop install --global firefox-developer
+Start PowerShell scoop install --global gimp
+Start PowerShell scoop install --global github
+Start PowerShell scoop install --global go
+Start PowerShell scoop install --global go-ipfs
+Start PowerShell scoop install --global ipfs-desktop
+Start PowerShell scoop install --global libreoffice-fresh
+Start PowerShell scoop install --global mediacreationtool
+Start PowerShell scoop install --global msiafterburner
+Start PowerShell scoop install --global nano
+Start PowerShell scoop install --global nodejs
+Start PowerShell scoop install --global openjdk
+Start PowerShell scoop install --global python
+Start PowerShell scoop install --global qbittorrent
+Start PowerShell scoop install --global rainmeter
+Start PowerShell scoop install --global rufus
+Start PowerShell scoop install --global steam
+Start PowerShell scoop install --global vlc
+Start PowerShell scoop install --global vscode-insiders
+Start PowerShell scoop install --global wget
+Start PowerShell scoop install --global minecraft
+Start PowerShell scoop install --global hyper
+Start PowerShell scoop install --global neofetch
+Start PowerShell scoop install --global make
+Start PowerShell scoop install --global ffmpeg
+Start PowerShell scoop install --global rocketchat-client
+Start PowerShell scoop install --global syncthing
+Start PowerShell scoop install --global terminus
+Start PowerShell scoop install --global touch
 
 Exit
