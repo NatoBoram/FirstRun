9400c0ad5167ebab5c655cbc49548d1dd3f055d9
Nato Boram <nato.boram+git@gmail.com>
2019-04-08 18:17:54 -0400

    moved around a few things

--- Linux/ElementaryOS.sh
+++ Linux/ElementaryOS.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 
 # Purge
-sudo apt purge -y --auto-remove evince gnome-font-viewer epiphany-browser
+sudo apt purge -y --auto-remove epiphany-browser evince gnome-font-viewer
 
 # Bionic
 curl https://repogen.simplylinux.ch/txt/bionic/sources_653e718beb5e3dec30278a9015bb420bad784294.txt | sudo tee /etc/apt/sources.list
 
-# Update
-sudo apt update
-sudo apt full-upgrade -y --auto-remove
-
 # Install
+sudo apt update
 sudo apt install -y --auto-remove adb aria2 baobab default-jdk fail2ban font-manager git git-lfs krita neofetch p7zip-full snapd software-properties-common steam unattended-upgrades youtube-dl
 
 # PPA
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo add-apt-repository -y ppa:openrazer/stable
 
+# Update
+sudo apt update
+sudo apt full-upgrade -y --auto-remove
+
 # Third-Party Drivers
-sudo apt install -y --auto-remove openrazer-meta
 sudo ubuntu-drivers autoinstall
+sudo apt install -y --auto-remove openrazer-meta
 sudo gpasswd -a $USER plugdev
 
 # Snaps
@@ -41,7 +42,6 @@ sudo snap install gnome-clocks
 sudo snap install gnome-contacts
 sudo snap install gnome-dictionary
 sudo snap install gnome-font-viewer
-sudo snap install gnome-font-viewer
 sudo snap install gnome-logs
 sudo snap install gnome-system-monitor
 sudo snap install go --classic
@@ -59,13 +59,13 @@ sudo snap install vlc
 
 # Connect
 sudo snap connect github-desktop:password-manager-service
-sudo snap connect glances:network
-sudo snap connect glances:system-observe
-sudo snap connect glances:mount-observe
 sudo snap connect glances:hardware-observe
 sudo snap connect glances:log-observe
+sudo snap connect glances:mount-observe
+sudo snap connect glances:network
 sudo snap connect glances:network-observe
 sudo snap connect glances:physical-memory-observe
+sudo snap connect glances:system-observe
 sudo snap connect glances:upower-observe
 sudo snap connect ipfs:removable-media
 
