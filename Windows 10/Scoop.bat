3a815209e87afcdd88fcfeada606e2bf81d51b1c
Nato Boram <NatoBoram@users.noreply.github.com>
2020-05-04 22:56:24 -0400

    add a bunch of programs

--- Windows 10/Scoop.bat
+++ Windows 10/Scoop.bat
@@ -34,10 +34,6 @@ Start PowerShell scoop install --global android-sdk
 Start PowerShell scoop install --global android-studio
 Start PowerShell scoop install --global autohotkey
 Start PowerShell scoop install --global curl
-Start PowerShell scoop install --global discord-canary
-Start PowerShell scoop install --global eclipse-java
-Start PowerShell scoop install --global filezilla
-Start PowerShell scoop install --global firefox-developer
 Start PowerShell scoop install --global gimp
 Start PowerShell scoop install --global github
 Start PowerShell scoop install --global go
@@ -49,7 +45,6 @@ Start PowerShell scoop install --global msiafterburner
 Start PowerShell scoop install --global nano
 Start PowerShell scoop install --global nodejs
 Start PowerShell scoop install --global openjdk
-Start PowerShell scoop install --global python
 Start PowerShell scoop install --global qbittorrent
 Start PowerShell scoop install --global rainmeter
 Start PowerShell scoop install --global rufus
@@ -58,14 +53,29 @@ Start PowerShell scoop install --global vlc
 Start PowerShell scoop install --global vscode-insiders
 Start PowerShell scoop install --global wget
 Start PowerShell scoop install --global minecraft
-Start PowerShell scoop install --global hyper
 Start PowerShell scoop install --global neofetch
 Start PowerShell scoop install --global make
 Start PowerShell scoop install --global ffmpeg
-Start PowerShell scoop install --global rocketchat-client
 Start PowerShell scoop install --global syncthing
-Start PowerShell scoop install --global terminus
 Start PowerShell scoop install --global touch
+
+start PowerShell scoop install -g dark
+start PowerShell scoop install -g dart
+start PowerShell scoop install -g dotnet-sdk
+start PowerShell scoop install -g ffmpeg
+start powershell scoop install -g freedownloadmanager
+start powershell scoop install -g gradle
+start powershell scoop install -g grep
+start powershell scoop install -g innounp
+start powershell scoop install -g less
+start powershell scoop install -g nssm
+start powershell scoop install -g openssl
+start powershell scoop install -g pwsh
+start powershell scoop install -g rust
+start powershell scoop install -g sed
+start powershell scoop install -g shasum
+start powershell scoop install -g webstorm
+start powershell scoop install -g youtube-dl
 
 REM Status
 scoop status
