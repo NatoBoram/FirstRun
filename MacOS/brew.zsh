58f7ed308fe4ba394075e0f82f2600afe9569109
Nato Boram <NatoBoram@users.noreply.github.com>
2021-11-12 22:49:16 -0500

    Update brew.zsh

--- MacOS/brew.zsh
+++ MacOS/brew.zsh
@@ -1,12 +1,22 @@
 #!/bin/zsh
 
+# Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Oh my ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-brew install aria2 wget xz gh neofetch git node p7zip curl go openjdk
+# PowerLevel10K
+brew install romkatv/powerlevel10k/powerlevel10k
+echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
+brew install aria2 gh git git-flow-avh go gpg neofetch node openjdk openssl p7zip shfmt wget
 
 brew install android-studio
+brew install free-download-manager
+brew install libreoffice
 brew install microsoft-edge
-brew install visual-studio-code-insiders
 brew install powershell-preview
+brew install steam
+brew install visual-studio-code-insiders
 brew install vlc
