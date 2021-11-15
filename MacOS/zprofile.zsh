3e869c5ec336f425f3552353c5f39a5a7cb50f7a
Nato Boram <nato.boram@gmail.com>
2021-11-15 14:36:59 -0500

    Sort zprofile

--- MacOS/zprofile.zsh
+++ MacOS/zprofile.zsh
@@ -4,12 +4,12 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 
-# Flutter
-PATH="$HOME/Library/flutter/bin:$PATH"
-
 # Android
 PATH="$HOME/Library/Android/sdk/tools/bin:$PATH"
 PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
+
+# Flutter
+PATH="$HOME/Library/flutter/bin:$PATH"
 
 # Go
 PATH="$HOME/Library/go/bin:$PATH"
