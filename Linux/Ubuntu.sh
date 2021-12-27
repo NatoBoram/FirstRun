70092e4848eeb9b824a71361dd05741ec8d4360f
Nato Boram <NatoBoram@users.noreply.github.com>
2021-12-27 12:17:49 -0500

    Use ZSH

--- Linux/Ubuntu.sh
+++ Linux/Ubuntu.sh
@@ -2,15 +2,21 @@
 
 # PPA
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
-# sudo add-apt-repository -y ppa:openrazer/stable
-sudo add-apt-repository ppa:kritalime/ppa
+sudo add-apt-repository -y ppa:inkscape.dev/stable
+sudo add-apt-repository -y ppa:kritalime/ppa
+#sudo add-apt-repository -y ppa:openrazer/stable
 
 # Update
 sudo apt update
 sudo apt full-upgrade -y --auto-remove
 
 # Install
-sudo apt install -y glances gnome-calendar htop gnome-dictionary gnome-clocks gnome-contacts adb aria2 baobab curl default-jdk font-manager git git-flow git-lfs gnome-tweaks neofetch p7zip-full steam youtube-dl
+sudo apt install -y adb aria2 baobab curl flatpak font-manager git git-flow git-lfs glances gnome-calendar gnome-clocks gnome-contacts gnome-tweaks htop neofetch p7zip-full steam youtube-dl zsh
+
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo ZSH_THEME="powerlevel10k/powerlevel10k" > ~/.zshrc
 
 # Third-Party Drivers
 sudo ubuntu-drivers autoinstall
@@ -23,20 +29,20 @@ sudo snap install android-studio --classic
 sudo snap install canonical-livepatch
 sudo snap install core
 sudo snap install core18
-sudo snap install ffmpeg
-sudo snap install gimp
-sudo snap install gnome-3-26-1604
-sudo snap install gnome-3-28-1804
-sudo snap install gnome-3-34-1804
 sudo snap install go --classic
 sudo snap install gtk-common-themes
-sudo snap install inkscape
-sudo snap install krita
 sudo snap install libreoffice
 sudo snap install node --edge --classic
-sudo snap install skype --classic
 sudo snap install snapcraft --classic
 sudo snap install vlc
+
+# Flatpak
+flatpak remote-add --if-not-exists --system elementary https://flatpak.elementary.io/repo.flatpakrepo
+flatpak remote-add --if-not-exists --system flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists --user elementary https://flatpak.elementary.io/repo.flatpakrepo
+flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install --system https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
 
 # Release Upgrade
 sudo do-release-upgrade
