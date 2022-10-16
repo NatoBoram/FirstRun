61646d885cb4006aed585959577c5733d119defc
Nato Boram <NatoBoram@users.noreply.github.com>
2022-10-16 13:50:10 -0400

    Create Nord.sh

---
+++ Linux/Nord.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+mkdir -p ~/Applications
+cd ~/Applications
+
+git clone git@github.com:vinceliuice/Graphite-gtk-theme.git
+cd Graphite-gtk-theme
+./install.sh --color dark --libadwaita --theme blue --tweaks nord --tweaks rimless
+cd ~/Applications
+
+git clone git@github.com:arcticicestudio/nord-gnome-terminal.git
+cd nord-gnome-terminal
+./src/install.sh
+cd ~/Applications
