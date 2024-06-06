b5ea402dfc8b0099e962181ae97a729fcb691f92
Nato Boram <NatoBoram@users.noreply.github.com>
2024-06-06 04:22:18 -0400

    zsh-syntax-highlighting

--- Linux/Ubuntu.sh
+++ Linux/Ubuntu.sh
@@ -22,6 +22,7 @@ chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo ZSH_THEME="powerlevel10k/powerlevel10k" >>~/.zshrc
 echo "# plugins=(zsh-autosuggestions)" >>~/.zshrc
 
