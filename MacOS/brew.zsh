31e57692de02f02de5449053055edc43337b1104
Nato Boram <NatoBoram@users.noreply.github.com>
2024-12-12 15:36:26 -0500

    Update brew.zsh

--- MacOS/brew.zsh
+++ MacOS/brew.zsh
@@ -8,7 +8,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # PowerLevel10K
 brew install romkatv/powerlevel10k/powerlevel10k
-echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 # Tap
 brew tap homebrew/cask-fonts
