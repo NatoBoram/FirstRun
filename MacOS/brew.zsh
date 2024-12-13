d96c722aeb5d426c8f1c741668a77aec0fdb1be5
Nato Boram <NatoBoram@users.noreply.github.com>
2024-12-12 20:22:30 -0500

    Update brew.zsh

--- MacOS/brew.zsh
+++ MacOS/brew.zsh
@@ -7,8 +7,10 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # PowerLevel10K
-brew install romkatv/powerlevel10k/powerlevel10k
+brew install romkatv/powerlevel10k/powerlevel10k zsh-autosuggestions zsh-syntax-highlighting zoxide
 echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >>~/.zshrc
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >>~/.zshrc
 
 # Formulae
 brew install aria2 gh git git-flow-avh go gpg neofetch node openjdk p7zip pnpm shfmt wget
